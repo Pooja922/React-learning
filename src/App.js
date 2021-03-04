7788e89c3bdca6d6a3377a907311768b8b204a2f
Pooja <shettypooja2000@gmail.com>
2021-03-04 14:50:10 +0530

    adding props

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
 //import {Greet} from './components/Greet' for named export
-import Hello from './components/Hello'
+//import Hello from './components/Hello'
 import Welcome from './components/Welcome'
 
 class App extends Component{
   render(){
     return(
     <div className="App">
-      <Greet name="Steve" heroName="Captain America"></Greet>
-      <Greet name="Tony" heroName="Iron Man"></Greet>
-      <Greet name="Bruce" heroName="Hulk"></Greet>
-      {/*<Welcome></Welcome>
-      <Hello />*/}
+      <Greet name="Steve" heroName="Captain America">
+        <p>This is a children class</p>
+      </Greet>
+      <Greet name="Tony" heroName="Iron Man">
+        <button>Action</button>
+      </Greet>
+      <Greet name="Bruce" heroName="Hulk">
+        <button>Action1</button>
+      </Greet>
+      <Welcome name="Steve" heroName="Captain America">
+        <button>Action2</button>
+      </Welcome>
+      <Welcome name="Tony" heroName="Iron Man">
+        <button>Action3</button>
+      </Welcome>
+      {/*<Hello />*/}
     </div>
     );
   }
